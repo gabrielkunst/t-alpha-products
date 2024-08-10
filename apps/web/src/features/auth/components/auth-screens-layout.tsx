e8b4a245bc3ecf233243c18f5954e964a7392fd3
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-08-09 23:54:43 -0300

    refactor: adjust min width to show right banner on auth

--- apps/web/src/features/auth/components/auth-screens-layout.tsx
+++ apps/web/src/features/auth/components/auth-screens-layout.tsx
@@ -3,10 +3,10 @@ import { PropsWithChildren } from 'react'
 export function AuthScreensLayout({ children }: PropsWithChildren) {
   return (
     <main className="flex min-h-screen">
-      <section className="flex w-full items-center justify-center px-12 py-6 md:w-1/3">
+      <section className="flex w-full items-center justify-center px-12 py-6 xl:w-1/3">
         {children}
       </section>
-      <section className="hidden w-2/3 bg-muted md:block" />
+      <section className="hidden w-2/3 bg-muted xl:block" />
     </main>
   )
 }
