fd69d3af5fc3f7a1b2200a5253cac6b7684da893
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-08-09 22:29:56 -0300

    feat: save login jwt to cookies

--- apps/web/src/features/auth/api/login.ts
+++ apps/web/src/features/auth/api/login.ts
@@ -1,11 +1,15 @@
 import { env } from '@alpha/env'
+import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 import { loginSchema } from '@/features/auth/utils'
 
+const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7
+
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json()
+    const cookiesStore = cookies()
     const validatedFields = loginSchema.safeParse(body)
 
     if (!validatedFields.success) {
@@ -19,12 +23,17 @@ export async function POST(request: NextRequest) {
     const response = await fetch(`${env.API_URL}/api/auth/login`, {
       body: JSON.stringify(body),
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
     })
 
     const json = await response.json()
+
+    cookiesStore.set('accessToken', json.data.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: ONE_WEEK_IN_SECONDS,
+    })
 
     return NextResponse.json({
       status: response.status,
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,9 +1,35 @@
+import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
+
+const AUTH_ROUTES = ['/auth/login', '/auth/register']
+const PRIVATE_ROUTES = ['/app']
 
 export function middleware(request: NextRequest) {
   const { pathname, origin } = request.nextUrl
+  const cookiesStore = cookies()
+  const accessToken = cookiesStore.get('accessToken')
 
   if (pathname === '/') {
-    return NextResponse.redirect(new URL('/auth/register', origin))
+    return NextResponse.redirect(new URL('/auth/login', origin))
   }
+
+  if (AUTH_ROUTES.includes(pathname)) {
+    if (!accessToken) {
+      return NextResponse.next()
+    }
+
+    return NextResponse.redirect(new URL('/app', origin))
+  }
+
+  if (PRIVATE_ROUTES.includes(pathname)) {
+    if (!accessToken) {
+      return NextResponse.redirect(new URL('/auth/login', origin))
+    }
+
+    return NextResponse.next()
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
